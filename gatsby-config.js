81bb341f946e3c3037aface6cece4dd93a4a1eab
Veerash Ayyagari <veeray@microsoft.com>
2021-01-06 15:41:52 -0800

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-06N8Z0RWJ4`,
       },
     },
     `gatsby-plugin-feed`,
